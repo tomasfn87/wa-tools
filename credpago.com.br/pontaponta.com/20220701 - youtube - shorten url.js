fd6d92c708a624be400eee74d96b1045cb663fad
tomasfn87 <tomas.nalle@alumni.usp.br>
2022-07-02 04:44:26 -0300

    Alterado o arquivo '//credpago.com.br/pontaponta.com/20220701 - youtube - shorten url.js': os testes foram reduzidos, tanto em relaçao ao número de linhas de código, quanto ao número de linhas impressas no console, por teste.

--- credpago.com.br/pontaponta.com/20220701 - youtube - shorten url.js
+++ credpago.com.br/pontaponta.com/20220701 - youtube - shorten url.js
@@ -18,15 +18,15 @@
 
 // teste
 (() => {
-    // let click_url = '{{Click URL}}'
     const reYouTubeEmbedUrl = /https?:\/\/www\.?youtube.com\/.*?embed\/([^?]+)\?\S*/
     const reYouTubeUrl = /https?:\/\/www\.youtube.com\/watch\?v=([^&]*)&?((feature\S+))*/
-    
-    const click_url_1 = 'https://www.youtube.com/embed/52uLdhW6qFk?enablejsapi=1&origin=https%3A%2F%2Fpontaponta.com'
-    const click_url_2 = 'https://www.youtube.com/watch?v=52uLdhW6qFk&feature=emb_imp_woyt'
-    const click_url_3 = 'https://www.youtube.com/watch?v=52uLdhW6qFk'
-    const test_list = [ click_url_1, click_url_2, click_url_3 ]
-    
+
+    const test_list = [
+        'https://www.youtube.com/embed/52uLdhW6qFk?enablejsapi=1&origin=https%3A%2F%2Fpontaponta.com',
+        'https://www.youtube.com/watch?v=52uLdhW6qFk&feature=emb_imp_woyt',
+        'https://www.youtube.com/watch?v=52uLdhW6qFk'
+    ]
+
     const test_yt_shorten_url = (n, click_url) => {
         console.log(' * Teste #'+String(Number(n)+1)+':')
         let video_id;
@@ -36,15 +36,15 @@
         else if (reYouTubeUrl.test(click_url)) {
             video_id = click_url.replace(reYouTubeUrl, '$1')
         }
-        console.log('de\n'+click_url+'\npara:')
+        let message = 'de: ' + click_url + '\npara: '
         if (video_id) {
-            console.log('if: youtu.be/' + video_id);
+            console.log(message + '(if) youtu.be/' + video_id);
         } else {
-            console.log('else: '+click_url.replace('https://www.youtube.com', 'youtu.be'))
+            console.log(message + '(else) ' + click_url.replace('https://www.youtube.com', 'youtu.be'))
         }
-        console.log()
     }
-    for (i in test_list) {
-        test_yt_shorten_url(i, test_list[i])
-    }
+    test_list.forEach((t, i) => {
+        i > 0 && console.log()
+        test_yt_shorten_url(i, t)
+    })
 })()
