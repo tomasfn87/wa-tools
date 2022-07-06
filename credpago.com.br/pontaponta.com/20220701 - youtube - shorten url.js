25583364ca7dbde79492930064d4d71c83c72fc9
Tomás <tomas.nalle@alumni.usp.br>
2022-07-06 11:01:18 -0300

    Atualizado arquivo 'credpago.com.br/pontaponta.com/20220701 - youtube - shorten url.js': 1): alterada a expressão regular da URL de vídeos do YouTube. 2): adicionado uma subexpressão regular, para verificar se o Video ID obtido corresponde ao formato usado pelo YouTube; caso contrário, a URL original será retornada.

--- credpago.com.br/pontaponta.com/20220701 - youtube - shorten url.js
+++ credpago.com.br/pontaponta.com/20220701 - youtube - shorten url.js
@@ -1,12 +1,13 @@
 () => {
     const click_url = '{{Click URL}}'
-    const reYouTubeUrl = /(https?:\/\/)?(www\.)?(youtube.com|youtu.be)\/(embed\/|watch\?v=)?([a-z0-9]{5,15})([\?&]\S*)*/i
+    const reYouTubeUrl = /(https?:\/\/)?(www\.)?((m\.)?youtube.com|youtu.be)\/(o?embed\/|watch\?v=)?([0-9A-Za-z_-]{10}[048AEIMQUYcgkosw])([\?&]\S*)*/
+    const reYouTubeVideoID = /[0-9A-Za-z_-]{10}[048AEIMQUYcgkosw]/
     let video_id
     
     if (reYouTubeUrl.test(click_url)) {
-        video_id = click_url.replace(reYouTubeUrl, '$5')
+        video_id = click_url.replace(reYouTubeUrl, '$6')
     }
-    if (video_id) {
+    if (reYouTubeVideoID.test(video_id)) {
         return 'youtu.be/' + video_id;
     }
     return click_url
@@ -14,16 +15,19 @@
 
 // teste
 (() => {
-    const reYouTubeUrl = /(https?:\/\/)?(www\.)?(youtube.com|youtu.be)\/(embed\/|watch\?v=)?([a-z0-9]{5,15})([\?&]\S*)*/i
+    const reYouTubeUrl = /(https?:\/\/)?(www\.)?((m\.)?youtube.com|youtu.be)\/(o?embed\/|watch\?v=)?([0-9A-Za-z_-]{10}[048AEIMQUYcgkosw])([\?&]\S*)*/
+    const reYouTubeVideoID = /[0-9A-Za-z_-]{10}[048AEIMQUYcgkosw]/
 
     const test_list = [
         'https://www.youtube.com/embed/52uLdhW6qFk?enablejsapi=1&origin=https%3A%2F%2Fpontaponta.com',
         'https://www.youtube.com/embed/52uLdhW6qFk?',
         'https://www.youtube.com/watch?v=52uLdhW6qFk&',
         'https://www.youtube.com/embed/52uLdhW6qFk',
+        'https://www.youtube.com/oembed/52uLdhW6qFk',
         'https://www.youtube.com/watch?v=52uLdhW6qFk&feature=emb_imp_woyt',
         'https://www.youtube.com/watch?v=52uLdhW6qFk',
         'https://youtube.com/watch?v=52uLdhW6qFk',
+        'https://m.youtube.com/watch?v=52uLdhW6qFk',
         'http://youtube.com/embed/52uLdhW6qFk',
         'www.youtube.com/watch?v=52uLdhW6qFk',
         'youtube.com/watch?v=52uLdhW6qFk',
@@ -36,10 +40,10 @@
         console.log(' * Teste #'+String(Number(n)+1)+':')
         let video_id;
         if (reYouTubeUrl.test(click_url)) {
-            video_id = click_url.replace(reYouTubeUrl, '$5')
+            video_id = click_url.replace(reYouTubeUrl, '$6')
         }
         let message = 'de: ' + click_url + '\npara: '
-        if (video_id) {
+        if (reYouTubeVideoID.test(video_id)) {
             console.log(message + '(Ok) youtu.be/' + video_id);
         } else {
             console.log(message + '(RegEx não pegou) ' + click_url)
