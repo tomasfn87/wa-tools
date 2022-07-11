10a6080cf061faeb299281dc4fde543b05bf9aca
Tomás <tomas.nalle@alumni.usp.br>
2022-07-11 16:40:09 -0300

    Atualizado arquivo '20220708 - simulador - header - rotulo.js'.

--- credpago.com.br/simulador/20220708 - simulador - header - rotulo.js
+++ credpago.com.br/simulador/20220708 - simulador - header - rotulo.js
@@ -1,18 +1,17 @@
 () => {
     let click_element = '{{Click Element}}'
-    let rotulo
-
+    let rotulo = ''
     // Logos
     if (click_element.querySelector('img')) {
+        rotulo += 'Logo - '
         if (click_element.querySelector('img').alt == 'Simulador de Locação CredPago') {
-            rotulo = 'Simulador de Locação CredPago'
+            rotulo += 'Simulador de Locação'
         } else if (click_element.querySelector('img').alt == 'Um produto CredPago') {
-            rotulo = 'CredPago'
+            rotulo += 'CredPago'
         }
     // NavBar
     } else {
         rotulo = click_element.innerText
     }
-
     return rotulo
 }
