54ad0ea4be1c4dac8ae73a57d9c92ea03f2888f4
Tomás <tomas.nalle@alumni.usp.br>
2022-07-12 10:22:13 -0300

    Atualizado arquivo '20220708 - simulador - header - rotulo.js': agora o arquivo extrai o rótulo do campo 'alt' de cada (antes os valores eram inseridos diretamente).

--- credpago.com.br/simulador/20220708 - simulador - header - rotulo.js
+++ credpago.com.br/simulador/20220708 - simulador - header - rotulo.js
@@ -1,13 +1,18 @@
 () => {
     let click_element = '{{Click Element}}'
+    let logos = document.querySelectorAll('header ul.style__LogosWrapper-qsk0em-1.fPZDmz li a img')
+    let logos_alt_text = []
+    logos.forEach((logo) => {
+        logos_alt_text.push(logo.alt)
+    })
     let rotulo = ''
     // Logos
     if (click_element.querySelector('img')) {
         rotulo += 'Logo - '
-        if (click_element.querySelector('img').alt == 'Simulador de Locação CredPago') {
-            rotulo += 'Simulador de Locação'
-        } else if (click_element.querySelector('img').alt == 'Um produto CredPago') {
-            rotulo += 'CredPago'
+        if (click_element.querySelector('img').alt == logos_alt_text[0]) {
+            rotulo += click_element.querySelector('img').alt.split(' CredPago')[0]
+        } else if (click_element.querySelector('img').alt == logos_alt_text[1]) {
+            rotulo += click_element.querySelector('img').alt.split('Um produto ')[1]
         }
     // NavBar
     } else {
