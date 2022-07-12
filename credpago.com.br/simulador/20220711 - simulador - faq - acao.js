9a36aa80e6ed10a46a3abf968547581ad7c97dc3
Tomás <tomas.nalle@alumni.usp.br>
2022-07-12 10:39:53 -0300

    Atualizado arquivo '20220711 - simulador - faq - acao.js': agora os valores retornados pela função são extraídos diretamente do conteúdo site, e não inseridos diretamente no código.

--- credpago.com.br/simulador/20220711 - simulador - faq - acao.js
+++ credpago.com.br/simulador/20220711 - simulador - faq - acao.js
@@ -1,15 +1,20 @@
 () => {
     const click_text = '{{Click Text}}'
     let acao
-    inquilino_h4 = document.querySelectorAll('#gatsby-focus-wrapper > div > main > div:nth-child(4) > div:nth-child(3) > div > div:nth-child(1) > div h4')
-    inquilino = []
+    const inquilino_h4 = document.querySelectorAll('main > div:nth-child(4) > div:nth-child(3) > div > div:nth-child(1) > div h4')
+    let inquilino = []
     inquilino_h4.forEach(i => inquilino.push(i.innerText))
-    imobiliaria_h4 = document.querySelectorAll('#gatsby-focus-wrapper > div > main > div:nth-child(4) > div:nth-child(3) > div > div:nth-child(2) > div h4')
-    imobiliaria = []
+    const imobiliaria_h4 = document.querySelectorAll('main > div:nth-child(4) > div:nth-child(3) > div > div:nth-child(2) > div h4')
+    let imobiliaria = []
     imobiliaria_h4.forEach(i => imobiliaria.push(i.innerText))
+    const faq_tipos = document.querySelectorAll('#faq li')
+    let faq_rotulos = []
+    faq_tipos.forEach((tipo) => {
+        faq_rotulos.push(tipo.innerText)
+    })
     inquilino.forEach((frase) => {
         if (frase == click_text) {
-            acao = 'Inquilino'
+            acao = faq_rotulos[0].split(' ')[1]
         }
     })
     if (acao) {
@@ -17,7 +22,7 @@
     }
     imobiliaria.forEach((frase) => {
         if (frase == click_text) {
-            acao = 'Imobiliária/Corretor'
+            acao = faq_rotulos[1].split(' ')[1]
         }
     })
     return acao
