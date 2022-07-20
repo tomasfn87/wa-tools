54b7da01247f0d171ad6fa3273095d4f2a053be4
Tomás <tomas.nalle@alumni.usp.br>
2022-07-20 15:18:17 -0300

    Alterado o arquivo 'euemdia.com.br/home/20220720 - home - footer - acao.js'.

--- euemdia.com.br/home/20220720 - home - footer - acao.js
+++ euemdia.com.br/home/20220720 - home - footer - acao.js
@@ -1,84 +1,77 @@
 () => {
     let click_text = '{{Click Text}}'
     click_text = click_text.trim()
-    let acao = ''
-    
-    titulos = []
-    titulosHtml = document.querySelectorAll('footer > div.MuiContainer-root.MuiContainer-maxWidthLg > div > div:nth-child(2) > div:nth-child(1) > div h6')
+    let titulos = []
+    const titulosHtml = document.querySelectorAll('footer > div.MuiContainer-root.MuiContainer-maxWidthLg > div > div:nth-child(2) > div:nth-child(1) > div h6')
     titulosHtml.forEach(t => titulos.push(t.innerText))
-    
-    sobreAEmDia = Array.from(document.querySelectorAll('footer div.MuiContainer-root.MuiContainer-maxWidthLg > div > div:nth-child(2) > div:nth-child(1) > div a')).slice(0,5)
-    
-    const mobile = navigator.userAgentData.mobile
-    if (mobile) {
-        minhasDividas = Array.from(document.querySelectorAll('footer div.MuiContainer-root.MuiContainer-maxWidthLg > div > div:nth-child(2) > div:nth-child(1) > div a')).slice(5,8)
+    const sobreAEmDiaHtml = Array.from(document.querySelectorAll('footer div.MuiContainer-root.MuiContainer-maxWidthLg > div > div:nth-child(2) > div:nth-child(1) > div a')).slice(0,5)
+    let minhasDividasHtml
+    if (navigator.userAgentData.mobile) {
+        minhasDividasHtml = Array.from(document.querySelectorAll('footer div.MuiContainer-root.MuiContainer-maxWidthLg > div > div:nth-child(2) > div:nth-child(1) > div a')).slice(5,8)
     } else {
-        minhasDividas = Array.from(document.querySelectorAll('footer div.MuiContainer-root.MuiContainer-maxWidthLg > div > div:nth-child(2) > div:nth-child(2) > div a'))
+        minhasDividasHtml = Array.from(document.querySelectorAll('footer div.MuiContainer-root.MuiContainer-maxWidthLg > div > div:nth-child(2) > div:nth-child(2) > div a'))
     }
-    
-    sobreAEmDia = []
+    let sobreAEmDia = []
     sobreAEmDiaHtml.forEach(i => {
         sobreAEmDia.push(i.innerText.trim())
     })
-    
-    minhasDividas = []
+    let minhasDividas = []
     minhasDividasHtml.forEach(i => {
         minhasDividas.push(i.innerText.trim())
     })
-    
-    sobreAEmDia.forEach(label => {
-        if (click_text == label) {
+    let acao = ''
+    sobreAEmDia.forEach(text => {
+        if (click_text == text) {
             acao += titulos[0]
         }
     })
     if (acao) {
         return acao
     }
-    minhasDividas.forEach(label => {
-        if (click_text == label) {
+    minhasDividas.forEach(text => {
+        if (click_text == text) {
             acao += titulos[1]
         }
     })
     return acao
 }
 
-// Para testar no console do navegador
-
+// Para testar no console do navegador:
 /* (() => {
     let click_text = 'Em Oferta! '
     click_text = click_text.trim()
-    let acao = ''
-    const mobile = navigator.userAgentData.mobile
     titulos = []
-    titulosHtml = document.querySelectorAll('footer > div.MuiContainer-root.MuiContainer-maxWidthLg > div > div:nth-child(2) > div:nth-child(1) > div h6')
+    const titulosHtml = document.querySelectorAll('footer > div.MuiContainer-root.MuiContainer-maxWidthLg > div > div:nth-child(2) > div:nth-child(1) > div h6')
     titulosHtml.forEach(t => titulos.push(t.innerText))
-    sobreAEmDiaHtml = Array.from(document.querySelectorAll('footer div.MuiContainer-root.MuiContainer-maxWidthLg > div > div:nth-child(2) > div:nth-child(1) > div a')).slice(0,5)
-    if (mobile) {
+    const sobreAEmDiaHtml = Array.from(document.querySelectorAll('footer div.MuiContainer-root.MuiContainer-maxWidthLg > div > div:nth-child(2) > div:nth-child(1) > div a')).slice(0,5)
+    let minhasDividasHtml
+    if (navigator.userAgentData.mobile) {
         minhasDividasHtml = Array.from(document.querySelectorAll('footer div.MuiContainer-root.MuiContainer-maxWidthLg > div > div:nth-child(2) > div:nth-child(1) > div a')).slice(5,8)
     } else {
         minhasDividasHtml = Array.from(document.querySelectorAll('footer div.MuiContainer-root.MuiContainer-maxWidthLg > div > div:nth-child(2) > div:nth-child(2) > div a'))
     }
-    sobreAEmDia = []
+    let sobreAEmDia = []
     sobreAEmDiaHtml.forEach(i => {
         sobreAEmDia.push(i.innerText.trim())
     })
-    minhasDividas = []
+    let minhasDividas = []
     minhasDividasHtml.forEach(i => {
         minhasDividas.push(i.innerText.trim())
     })
-    sobreAEmDia.forEach(label => {
-        if (click_text == label) {
+    let acao = ''
+    sobreAEmDia.forEach(text => {
+        if (click_text == text) {
             acao += titulos[0]
         }
     })
     if (acao) {
-        console.log(acao)
+        console.log(`Ação: ${acao}`)
         return
     }
-    minhasDividas.forEach(label => {
-        if (click_text == label) {
+    minhasDividas.forEach(text => {
+        if (click_text == text) {
             acao += titulos[1]
         }
     })
-    console.log(acao)
+    console.log(`Ação: ${acao}`)
 })() */
