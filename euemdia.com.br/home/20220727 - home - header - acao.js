66df772eabf874c3746a2dec6a533759048a10b9
tomasfn87 <tomas.nalle@alumni.usp.br>
2022-07-27 20:57:09 -0300

    Alterados os arquivos da pasta 'euemdia.com.br/home': '20220727 - home - header - acao.js' e '20220727 - home - header - rotulo.js': as strings de seletores CSS foram declaradas em variáveis para permitir uma leitura mais clara do código.

--- euemdia.com.br/home/20220727 - home - header - acao.js
+++ euemdia.com.br/home/20220727 - home - header - acao.js
@@ -1,12 +1,17 @@
 () => {
-    const e = '{{Click Element}}'
-    if (e.matches('header nav > div:nth-child(1) > div > h1 > svg')) {
+    const click_element = '{{Click Element}}'
+    const cssLogoEmDia = 'header nav > div:nth-child(1) > div > h1 > svg'
+    const cssBotaoEntrar = '#root > div > div > div > header > div > div > nav > div:nth-of-type(2) span'
+    const cssBotao2aViaDeBoleto = 'header nav > div:nth-child(1) > div > h1 + div'
+    const cssMenuHamburguer = 'header nav > div:nth-child(3) div > a > div:nth-of-type(1)'
+    const cssMenuOQueVocePrecisa = '#root > div > div > div > header > div > div > nav > div > div > div > div > a > div:nth-of-type(1)'
+    if (click_element.matches(cssLogoEmDia)) {
       return 'Logo' 
-    } else if (e.matches('#root > div > div > div > header > div > div > nav > div:nth-of-type(2) span') || e.matches('header nav > div:nth-child(1) > div > h1 + div')) {
+    } else if (click_element.matches(cssBotaoEntrar) || click_element.matches(cssBotao2aViaDeBoleto)) {
       return 'Botão'
-    } else if (e.matches('header nav > div:nth-child(3) div > a > div:nth-of-type(1)')) {
+    } else if (click_element.matches(cssMenuHamburguer)) {
       return 'Menu hambúrguer'
-    } else if (e.matches('#root > div > div > div > header > div > div > nav > div > div > div > div > a > div:nth-of-type(1)')) {
+    } else if (click_element.matches(cssMenuOQueVocePrecisa)) {
       return 'Menu O que você precisa?'
     }
   }
--- euemdia.com.br/home/20220727 - home - header - rotulo.js
+++ euemdia.com.br/home/20220727 - home - header - rotulo.js
@@ -1,8 +1,9 @@
-function () {
-    const click_element = '{{Click Element}}';
-    if (click_element.matches('header nav > div:nth-child(1) > div > h1 > svg')) {
-      return 'emDia'
+() => {
+    const click_element = '{{Click Element}}'
+    const cssLogoEmDia = 'header nav > div:nth-child(1) > div > h1 > svg'
+    if (click_element.matches(cssLogo)) {
+        return 'emDia'
     } else {
-      return '{{Click Text}}'
+        return '{{Click Text}}'
     }
-  }
+}
