a158485646d31760732fb0cb64f1e9d0bd9285c3
Tomás <tomas.nalle@alumni.usp.br>
2022-07-27 21:09:26 -0300

    Alterado arquivo 'euemdia.com.br/home/20220727 - home - header - rotulo.js': corrigido nome de variável.

--- euemdia.com.br/home/20220727 - home - header - rotulo.js
+++ euemdia.com.br/home/20220727 - home - header - rotulo.js
@@ -1,7 +1,7 @@
 () => {
     const click_element = '{{Click Element}}'
     const cssLogoEmDia = 'header nav > div:nth-child(1) > div > h1 > svg'
-    if (click_element.matches(cssLogo)) {
+    if (click_element.matches(cssLogoEmDia)) {
         return 'emDia'
     } else {
         return '{{Click Text}}'
