54555f9cef588a13283badc5d0ae6e64fc3d062a
Tomás <tomas.nalle@alumni.usp.br>
2022-08-01 17:08:25 -0300

    Alterado arquivo 'euemdia.com.br/home/20220727 - home - header - rotulo.js'.

--- euemdia.com.br/home/20220727 - home - header - rotulo.js
+++ euemdia.com.br/home/20220727 - home - header - rotulo.js
@@ -2,7 +2,8 @@
     const ce = '{{Click Element}}'
     if (ce.matches('{{[RCK] Home - Header - Logo emDia (CSS)}}')) {
         return 'emDia'
-    } else if (ce.matches('{{[RCK] Home (login) - Header - Menu usuário - Ícone Sair (CSS)}}')) {
+    } else if (ce.matches('{{[RCK] Home (login) - Header - Menu usuário - Ícone Sair (desktop) (CSS)}}')
+        || ce.matches('{{[RCK] Home - Header - Menu hambúrguer (mobile) - Ícone Sair (CSS)}}')) {
         return 'SAIR'
     }
     else {
