6c4a56bbbe13b5da563f3ee65abadb3c3bcf6d72
Tomás <tomas.nalle@alumni.usp.br>
2022-08-01 11:56:51 -0300

    Alterado arquivo 'euemdia.com.br/home/20220728 - home - footer - rotulo.js'.

--- euemdia.com.br/home/20220728 - home - footer - rotulo.js
+++ euemdia.com.br/home/20220728 - home - footer - rotulo.js
@@ -2,7 +2,9 @@
     const ce = '{{Click Element}}'
     if (ce.matches('{{[RCK] Home - Footer - Cliques de saída - Redes sociais (CSS)}}')) {
         return '{{Click URL}}'
-    } else if (ce.matches('{{[RCK] Home - Footer - Links (CSS)}}')) {
+    } else if (ce.matches('{{[RCK] Home - Footer - Políticas e segurança / Procon (CSS)}}')) {
+        return '{{[RCK] Home - Footer - Políticas e segurança / Procon - Rótulo}}'
+    } else {
         return '{{Click Text}}'
     }
 }
