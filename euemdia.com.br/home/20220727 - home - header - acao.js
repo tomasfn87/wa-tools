5c5d494339debcb865a674659f7a677ddf504101
Tomás <tomas.nalle@alumni.usp.br>
2022-08-01 17:04:00 -0300

    Alterado arquivos da pasta 'euemdia.com.br/home/': '20220719 - home - acionadores.txt' e 'euemdia.com.br/home/20220727 - home - header - acao.js'.

--- euemdia.com.br/home/20220727 - home - header - acao.js
+++ euemdia.com.br/home/20220727 - home - header - acao.js
@@ -5,14 +5,18 @@
     } else if (ce.matches('{{[RCK] Home - Header - Botão Entrar (desktop) (CSS)}}') 
         || ce.matches('{{[RCK] Home - Header - Botão 2a via de boleto (mobile) (CSS)}}')) {
         return 'Botão'
-    } else if (ce.matches('{{[RCK] Home - Header - Menu hambúrguer (mobile) (CSS)}}')) {
+    } else if (ce.matches('{{[RCK] Home - Header - Menu hambúrguer (mobile) (CSS)}}')
+        || ce.matches('{{[RCK] Home - Header - Menu hambúrguer (mobile) - Alterar sua conta - Texto (CSS)}}')
+        || ce.matches('{{[RCK] Home - Header - Menu hambúrguer (mobile) - Ícone Sair (CSS)}}')) {
         return 'Menu hambúrguer'
     } else if (ce.matches('{{[RCK] Home - Header - Menu O que você precisa? (desktop) (CSS)}}')) {
         return 'Menu O que você precisa?'
     } else if (ce.matches('{{[RCK] Home (login) - Header - Menu Minhas Dívidas (desktop) (CSS)}}')) {
         return 'Menu Minhas Dívidas'
-    } else if (ce.matches('{{[RCK] Home (login) - Header - Menu usuário (CSS)}}')
-        || ce.matches('{{[RCK] Home (login) - Header - Menu usuário - Ícone Sair (CSS)}}')) {
+    } else if (ce.matches('{{[RCK] Home (login) - Header - Menu usuário (desktop) (CSS)}}')
+        || ce.matches('{{[RCK] Home (login) - Header - Menu usuário - Ícone Sair (desktop) (CSS)}}')) {
         return 'Menu usuário'
+    } else if (ce.matches('{{[RCK] Home - Header - Menu hambúrguer (mobile) - Subitens (CSS)}}')) {
+        return 'Menu hambúrguer - ' + '{{[RCK] Home - Header - Menu hambúrguer (mobile) - Subitens - Ação}}'
     }
 }
