75f5635c5fb833ea56cf29c79f7d2e0b6fd32ed1
Tomás <tomas.nalle@alumni.usp.br>
2022-08-01 11:56:20 -0300

    Alterado arquivo 'euemdia.com.br/home/20220728 - home - footer - links - acao.js'.

--- euemdia.com.br/home/20220728 - home - footer - links - acao.js
+++ euemdia.com.br/home/20220728 - home - footer - links - acao.js
@@ -1,52 +1,49 @@
-function () {
-    var ce = {{Click Element}};
-    var findFirstPreviousTag = function (tagElement, tagName, levelsUp) {
+() => {
+    const ce = '{{Click Element}}';
+    const findFirstPreviousTag = (tagElement, tagName, levelsUp) => {
         if (!findFirstPreviousSiblingTag(tagElement, tagName)) {
-            c = 0;
+            c = 0
             while (c <= levelsUp && tagElement.parentNode) {
                 if (findFirstPreviousSiblingTag(tagElement, tagName)) {
-                    return findFirstPreviousSiblingTag(tagElement, tagName);
+                    return findFirstPreviousSiblingTag(tagElement, tagName)
                 }
-                tagElement = tagElement.parentNode;
-                c++;
+                tagElement = tagElement.parentNode
+                c++
             }
+        } else {
+            return findFirstPreviousSiblingTag(tagElement, tagName)
         }
-        else {
-            return findFirstPreviousSiblingTag(tagElement, tagName);
-        }
-    };
-    var findFirstPreviousSiblingTag = function (tagElement, tagName) {
-        var s = (0, getOrderedSiblingTags)(tagElement, 'previous');
+    }
+    const findFirstPreviousSiblingTag = (tagElement, tagName) => {
+        let s = getOrderedSiblingTags(tagElement, 'previous')
         if (s.siblings.previous) {
-            for (var _i = 0, _a = s.siblings.previous.reverse(); _i < _a.length; _i++) {
-                var tag = _a[_i];
+            for (let tag of s.siblings.previous.reverse()) {
                 if (tag.tagName.toLowerCase() == tagName.toLowerCase()) {
-                    return tag;
+                    return tag
                 }
             }
         }
-    };
-    var getOrderedSiblingTags = function (tagElement, s) {
-        if (s === void 0) {
-            s = 'both';
-        }
-        var orderedSiblings = {
-            tag: tagElement,
+    }
+    const getOrderedSiblingTags = (tagElement, s='both') => {
+        // Main object
+        let orderedSiblings = {
+            tag: tagElement, 
             siblings: {}
-        };
-        var sCheck = ['both', 'previous'];
-        if (!sCheck.includes(s)) {
-            s = 'both';
         }
-        if ((s == 'both' || s == 'previous') && tagElement.previousElementSibling) {
-            orderedSiblings.siblings.previous = [];
-            currentTag = tagElement;
+        const sCheck = ['both', 'previous']
+        if (!sCheck.includes(s)) {
+            s = 'both'
+        }
+        // Previous siblings
+        if ((s=='both' || s=='previous') && tagElement.previousElementSibling) {
+            orderedSiblings.siblings.previous = []
+            currentTag = tagElement
             while (currentTag.previousElementSibling) {
-                orderedSiblings.siblings.previous.unshift(currentTag.previousElementSibling);
-                currentTag = currentTag.previousElementSibling;
+                orderedSiblings.siblings.previous.unshift(currentTag.previousElementSibling)
+                currentTag = currentTag.previousElementSibling
             }
         }
-        return orderedSiblings;
-    };
+        return orderedSiblings
+    }
     return findFirstPreviousTag(ce, 'h6', 2).innerText;
 }
