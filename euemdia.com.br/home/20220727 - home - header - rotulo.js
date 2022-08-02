0f9ed9981ba5befcdd80da1601402b6accf3b9a6
Tomás <tomas.nalle@alumni.usp.br>
2022-08-02 11:32:44 -0300

    Alterado arquivo '20220727 - home - header - rotulo.js': alterado nome de variável do GTM.

--- euemdia.com.br/home/20220727 - home - header - rotulo.js
+++ euemdia.com.br/home/20220727 - home - header - rotulo.js
@@ -2,7 +2,7 @@
     const ce = '{{Click Element}}'
     if (ce.matches('{{[RCK] Home - Header - Logo emDia (CSS)}}')) {
         return 'emDia'
-    } else if (ce.matches('{{[RCK] Home (login) - Header - Menu usuário - Ícone Sair (desktop) (CSS)}}')
+    } else if (ce.matches('{{[RCK] Home (login) - Header - Menu usuário (desktop) - Ícone Sair (CSS)}}')
         || ce.matches('{{[RCK] Home - Header - Menu hambúrguer (mobile) - Ícone Sair (CSS)}}')) {
         return 'SAIR'
     }
