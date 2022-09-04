1752123e69707aa1ea1eabbc1beb4501c591ba3f
Tomás Foch Nalle <tomas.nalle@alumni.usp.br>
2022-09-04 10:58:23 -0300

    Changed files from folder 'joinUniqueElementsFromArrs': 1): 'joinUniqueElementsFromArrs.js' and 2): 'test/joinUniqueElementsFromArrs.test.js': 1): changed 'joinArrs' declaration from '[]' to 'new Array'. 2): changed the order of the tests.

--- joinUniqueElementsFromArrs/joinUniqueElementsFromArrs.js
+++ joinUniqueElementsFromArrs/joinUniqueElementsFromArrs.js
@@ -1,6 +1,6 @@
 const joinUniqueElementsFromArrs = (...arrArr) => {
   if (arrArr.length < 1) return new Array;
-  const joinedArrs = [];
+  const joinedArrs = new Array;
   for (let arr of arrArr) {
     for (let item of arr) {
       !joinedArrs.includes(item) && joinedArrs.push(item);
