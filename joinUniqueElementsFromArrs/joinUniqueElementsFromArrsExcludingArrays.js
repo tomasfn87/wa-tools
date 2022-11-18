16eb4c3b30da84246d76a69e8fd00ffac27bfa25
Tomás Foch Nalle <tomas.nalle@alumni.usp.br>
2022-11-18 07:33:44 -0300

    Added files to folder 'findPreviousOrNextTag': added files 'joinUniqueElementsFromArrsExcludingArrays.js' and 'test/joinUniqueElementsFromArrsExcludingArrays.test.js': it's a variant of 'joinUniqueElementsFromArrsExcludingObjects.js', but it will only skip arrays (instead of also skipping any kind of Object (including null objects).

---
+++ joinUniqueElementsFromArrs/joinUniqueElementsFromArrsExcludingArrays.js
@@ -0,0 +1,12 @@
+const joinUniqueElementsFromArrsExcludingArrays = (...arrArr) => {
+  if (arrArr.length < 1) return new Array;
+  const joinedArrs = new Array;
+  for (let arr of arrArr) {
+    for (let item of arr) {
+       !Array.isArray(item) && !joinedArrs.includes(item) && joinedArrs.push(item)
+     }
+  }
+  return joinedArrs;
+}
+
+export default joinUniqueElementsFromArrsExcludingArrays;
