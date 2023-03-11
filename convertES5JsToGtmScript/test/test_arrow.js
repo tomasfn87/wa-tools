bed742e557d58ea80f215ba9d1ed9b171185edc4
Tomás Foch Nalle <tomas.nalle@alumni.usp.br>
2023-03-11 15:10:29 -0300

    Changed files from folder 'convertES5JsToGtmScript/test': removed non-automatized test files.

--- convertES5JsToGtmScript/test/test_arrow.js
+++
@@ -1,7 +0,0 @@
-() => {
-    const click_url = '{{Click URL}}'
-    if (click_url) {
-        return click_url
-    }
-    return ''
-}
--- convertES5JsToGtmScript/test/teste.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    var tst = {
-        a: '{{teste}}',
-        b: "{{TESTE}}",
-        c: '{{[ONE]More teste}}',
-        d: "{{[ONE]LAST t-E-S-t}}"
-    };
-    console.log(tst);
-})();
