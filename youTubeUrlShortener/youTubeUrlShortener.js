87d659ebedd82f156a789fff1e2e3ba13e0393cd
Tomás Foch Nalle <tomas.nalle@alumni.usp.br>
2023-11-26 15:49:21 -0300

    Alterado arquivo 'youTubeUrlShortener/youTubeUrlShortener.js': simplificadas as expressões regulares.

--- youTubeUrlShortener/youTubeUrlShortener.js
+++ youTubeUrlShortener/youTubeUrlShortener.js
@@ -1,20 +1,20 @@
 const youTubeUrlShortener = (ytUrl) => {
-    if (!ytUrl) return "";
-    const reYouTubeUrl = /(https?:\/\/)?(www\.)?((m\.)?youtube.com|youtu.be)\/(o?embed\/|watch\?v=)?([0-9A-Za-z_-]{10}[048AEIMQUYcgkosw])([\?&]\S*)*/;
-    const reYouTubeVideoID = /[0-9A-Za-z_-]{10}[048AEIMQUYcgkosw]/;
-    let video_id;
+    if (!ytUrl) return ""
+    const reYouTubeUrl = /(https?:\/\/)?(www\.)?(m\.)?youtu(\.be|be\.com)\/(o?embed\/|watch\?v=)?([0-9A-Za-z_-]{10}[048AEIMQUYcgkosw])([\?&]\S*)*/
+    const reYouTubeVideoID = /[0-9A-Za-z_-]{10}[048AEIMQUYcgkosw]/
+    let video_id
     if (reYouTubeUrl.test(ytUrl)) {
-        video_id = ytUrl.replace(reYouTubeUrl, '$6');
+        video_id = ytUrl.replace(reYouTubeUrl, '$6')
     }
     if (reYouTubeVideoID.test(video_id)) {
-        return 'youtu.be/' + video_id;
+        return 'youtu.be/' + video_id
     }
-    return ytUrl;
+    return ytUrl
 }
 
-export default youTubeUrlShortener;
+export default youTubeUrlShortener
 
 if (process.argv.length > 2) {
-    let youTubeUrl = process.argv[2];
-    console.log("https://" + youTubeUrlShortener(youTubeUrl));
+    let youTubeUrl = process.argv[2]
+    console.log("https://" + youTubeUrlShortener(youTubeUrl))
 }
